cdb9fbf872d49d0efabe4db5fd1915bab5c348f2
ShinWolf <77382557+ShinWolf@users.noreply.github.com>
2024-05-30 12:49:45 +0200

    Observable switchMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { Observable, concatMap, delay, exhaustMap, filter, interval, map, mergeMap, of, take, tap } from 'rxjs';
+import { Observable, concatMap, delay, exhaustMap, filter, interval, map, mergeMap, of, switchMap, take, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -25,8 +25,8 @@ export class AppComponent implements OnInit {
       take(10),
       map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
       tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-      // Tant que la requète n'est pas terminé,selle ignore toutes les requètes reçu entre le debut de la première jusqu'a la fin de son traitement
-      exhaustMap(color => this.getTrainObservable$(color)),
+      // Il change la requete en cours si une nouvelle requète est reçu
+      switchMap(color => this.getTrainObservable$(color)),
       tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
     ).subscribe();
   }
