6d0242c01ddeb78ee6dfe6bb73f5e4c26046f8a8
ShinWolf <77382557+ShinWolf@users.noreply.github.com>
2024-05-30 13:05:53 +0200

    unsubscribe first soluce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { Observable, concatMap, delay, exhaustMap, filter, interval, map, mergeMap, of, switchMap, take, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,33 +14,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-
-  redTrainsCalled = 0;
-  yellowTrainsCalled = 0;
-
-  ngOnInit() {
-    interval(500).pipe(
-      take(10),
-      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
-      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-      // Il change la requete en cours si une nouvelle requète est reçu
-      switchMap(color => this.getTrainObservable$(color)),
-      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
-    ).subscribe();
-  }
-
-  getTrainObservable$(color: 'rouge' | 'jaune') {
-    const isRedTrain = color === 'rouge';
-    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
-    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
-    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
-    return of({ color, trainIndex }).pipe(
-      delay(isRedTrain ? 5000 : 6000)
-    );
-  }
-
-  translateColor(color: 'rouge' | 'jaune') {
-    return color === 'rouge' ? 'red' : 'yellow';
-  }
-}
+export class AppComponent { }
--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapComponent } from "../face-snap/face-snap.component";
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { interval, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -17,5 +18,11 @@ export class FaceSnapListComponent implements OnInit {
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getFaceSnaps();
+
+    interval(1000).pipe(
+      // Take si on connais le nombre de valeur a récupère mais si on change de component il sera tjr présent
+      take(1),
+      tap(console.log)
+    ).subscribe();
   }
 }
