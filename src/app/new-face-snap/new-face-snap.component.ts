539270338302ce8938e680f2abef1cd84c7334a4
ShinWolf <77382557+ShinWolf@users.noreply.github.com>
2024-06-03 20:34:17 +0200

    Creation de formulaire reactif

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { FaceSnap } from '../models/face-snap';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-face-snap',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './new-face-snap.component.html',
+  styleUrl: './new-face-snap.component.scss'
+})
+export class NewFaceSnapComponent implements OnInit {
+  snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.snapForm = this.formBuilder.group({
+      title: [null],
+      description: [null],
+      imageUrl: [null],
+      location: [null]
+    });
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+        createdAt: new Date(),
+        id: 0,
+        snaps: 0
+      }))
+    );
+  }
+
+  onSubmitForm(): void {
+    console.log(this.snapForm.value);
+  }
+}
