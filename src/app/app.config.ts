e4d8d058c3c6b40dccf52263a902c936d3f1dc37
ShinWolf <77382557+ShinWolf@users.noreply.github.com>
2024-06-03 18:06:25 +0200

    Ajout de template form

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,5 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), { provide: LOCALE_ID, useValue: 'fr-FR' }]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), { provide: LOCALE_ID, useValue: 'fr-FR' }],
 };
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
+  userEmail!: string;
+
   constructor(private router: Router) { }
 
   onContinue() {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm): void {
+    console.log(form.value);
   }
 }
